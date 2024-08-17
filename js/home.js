c11e996d98bb2ebc193563eac0980534f35421e5
phucx0 <155630182+phucx0@users.noreply.github.com>
2024-08-17 14:35:23 +0700

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,36 @@
+document.getElementById('tiktok').addEventListener('click',()=>{
+    window.location.href ="https://www.tiktok.com/@check.coin"
+})
+document.getElementById('youtube').addEventListener('click',()=>{
+    window.location.href ="https://www.youtube.com/@checkcoinsoftware"
+})
+document.getElementById('telegram').addEventListener('click',()=>{
+    window.location.href ="https://t.me/checkcoin"
+})
+document.getElementById('support').addEventListener('click',()=>{
+    window.location.href ="https://t.me/kevin2qq"
+})
+document.getElementById('buy-now').addEventListener('click',()=>{
+    window.location.href ="https://t.me/kevin2qq"
+})
+
+
+document.getElementById('lean-more-pc').addEventListener('click',()=>{
+    document.querySelector('.body').style.opacity = "0.1";
+    document.getElementById('pc').style.display = "flex";
+})
+
+document.getElementById('back-1').addEventListener('click',()=>{
+    document.getElementById('pc').style.display = "none";
+    document.querySelector('.body').style.opacity = "1";
+})
+
+document.getElementById('lean-more-phone').addEventListener('click',()=>{
+    document.querySelector('.body').style.opacity = "0.1";
+    document.getElementById('phone').style.display = "flex";
+})
+
+document.getElementById('back-2').addEventListener('click',()=>{
+    document.getElementById('phone').style.display = "none";
+    document.querySelector('.body').style.opacity = "1";
+})
